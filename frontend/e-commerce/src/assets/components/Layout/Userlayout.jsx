4f334c3ba03399aed5c479212fba74b0c68e86ae
HumayunImtiaz <Humayunimtiaz81@gmail.com>
2025-05-14 22:02:59 -0700

    Footer Sectioc Complete

--- frontend/e-commerce/src/assets/components/Layout/Userlayout.jsx
+++ frontend/e-commerce/src/assets/components/Layout/Userlayout.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Header from '../Common/Header'
+import Footer from '../Common/Footer'
 
 function Userlayout() {
   return (
     <div>
+        {/* Header file mai hmara topbar or navbar hai */}
         <Header/>
+
+        <Footer/>
     </div>
   )
 }
