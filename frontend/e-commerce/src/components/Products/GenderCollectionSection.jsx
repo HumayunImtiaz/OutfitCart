bd2ff6417ffeaa524e11b984b448d1b5b2462294
HumayunImtiaz <Humayunimtiaz81@gmail.com>
2025-05-16 00:54:39 -0700

    Gender Section Complete

---
+++ frontend/e-commerce/src/components/Products/GenderCollectionSection.jsx
@@ -0,0 +1,47 @@
+import { Link } from 'react-router-dom'
+import mensCollectionImage from '../../assets/mens-collection.webp'
+import womensCollectionImage from '../../assets/womens-collection.webp'
+function GenderCollectionSection() {
+  return (
+    <section className='py-10 px-4 lg:px-0 '>
+        <div className='container mx-auto flex flex-col md:flex-row gap-8'>
+            {/* Womens Collection */}
+            <div className='relative flex-1'>
+                <img src={womensCollectionImage} alt="Women's Collection"
+                className='w-full h-[600px] object-cover' 
+                />
+                <div className='absolute bottom-8 left-8 bg-white/90 p-4'>
+                 <h2 className='text-2xl font-bold text-gray-900 mb-3'>
+                    Women's Collections
+                 </h2>
+                 <Link to="/collections/all?gender=women"
+                 className='text-gray-900 underline'>
+                    Shop Now
+                 </Link>
+                </div>
+            </div>
+            {/* Mwn's Collections */}
+
+             <div className='relative flex-1'>
+                <img src={mensCollectionImage} alt="Women's Collection"
+                className='w-full h-[600px] object-cover' 
+                />
+                <div className='absolute bottom-8 left-8 bg-white/90 p-4'>
+                 <h2 className='text-2xl font-bold text-gray-900 mb-3'>
+                    Men's Collections
+                 </h2>
+                 <Link to="/collections/all?gender=men"
+                 className='text-gray-900 underline'>
+                    Shop Now
+                 </Link>
+                </div>
+            </div>
+
+
+        </div>
+
+    </section>
+  )
+}
+
+export default GenderCollectionSection
