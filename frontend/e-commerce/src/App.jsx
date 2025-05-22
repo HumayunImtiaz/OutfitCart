7c2ed68a5ed6df50c866d62b9cfe30d42a4e250c
HumayunImtiaz <Humayunimtiaz81@gmail.com>
2025-05-21 18:44:00 -0700

    Product Grid and Top Wear for Women section completed

--- frontend/e-commerce/src/App.jsx
+++ frontend/e-commerce/src/App.jsx
@@ -3,10 +3,11 @@ import './App.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Userlayout from './components/Layout/Userlayout'
 import Home from './pages/Home'
-
+import {Toaster} from "sonner";
 function App() {
   return (
     <BrowserRouter>
+    <Toaster position="top-right"/>
     <Routes>
       <Route path='/'  element={<Userlayout/>}>
       {/* Ye child route hai jisy hm Userlayout.jsx mai Outlet ky zrye access kry gy */}
--- frontend/e-commerce/src/components/Products/ProductDetails.jsx
+++ frontend/e-commerce/src/components/Products/ProductDetails.jsx
@@ -1,156 +1,237 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { toast } from "sonner";
+import ProductGrid from "./ProductGrid";
+
+const selectedProduct = {
+    name: "Stylish Jacket",
+    price: 120,
+    OriginalPrice: 150,
+    description: "This is the Stylish Jacket perfect for any accassion",
+    brand: "FasionBran",
+    material: "Lather",
+    sizes: ["S", "M", "L", "XL"],
+    colors: ["Red", "Black"],
+    images: [
+      {
+        url: "https://picsum.photos/500/500?random=1",
+        altText: "Stylish Jacket",
+      },
+      {
+        url: "https://picsum.photos/500/500?random=2",
+        altText: "Stylish Jacket",
+      },
+    ],
+  };
+
+//  similar product data for ProductsGrid.jsx
+
+const similarProducts=[
+    {
+        _id:1,
+        name:"Product 1",
+        price:100,
+        image:[{url: "https://picsum.photos/500/500?random=1",}]
+    },
+    {
+        _id:2,
+        name:"Product 1",
+        price:100,
+        image:[{url: "https://picsum.photos/500/500?random=2",}]
+    },
+    {
+        _id:3,
+        name:"Product 2",
+        price:100,
+        image:[{url: "https://picsum.photos/500/500?random=3",}]
+    },
+    {
+        _id:4,
+        name:"Product 3",
+        price:100,
+        image:[{url: "https://picsum.photos/500/500?random=4",}]
+    },
+   
+]
 
 
 
 function ProductDetails() {
+    const [mainImage, setmainImage]= useState("")
+    const[selectSize, setSelectSize]=useState("")
+    const[selectColor, setSelectColor]=useState("")
+    const [quantity, setQuantiity]= useState(1)
+    const[isButtonDisabled,  setIsButtonDisabled]=useState(false)
 
-    const selectedProduct={
-        name:"Stylish Jacket",
-        price: 120,
-        OriginalPrice:150,
-        description: "This is the Stylish Jacket perfect for any accassion",
-        brand:"FasionBran",
-        material:"Lather",
-        sizes:['S',"M","L","XL"],
-        colors:["Red","Black"],
-        image:[
-            {
-            url:"https://picsum.photos/500/500?random=1",
-            altText:"Stylish Jacket"
-        },
-        {
-            url:"https://picsum.photos/500/500?random=1",
-            altText:"Stylish Jacket"
+    const handleQuantity=((action)=>{
+        if(action==="minus" && quantity>1)  setQuantiity((prev)=> prev-1)
+        if(action==="plus")  setQuantiity((prev)=> prev+1)
+    })
+
+    const handleAddToCart=()=>{
+        if(!selectSize || !selectColor){
+            toast.error("Please Select a size and color before adding" ,{
+                duration: 1000,
+            });
+            return
         }
-    
-    ]
-
+        setIsButtonDisabled(true)
+        
+        setTimeout(()=>{
+            toast.success("Product added to Cart",{
+                duration:1000,
+            })
+            setIsButtonDisabled(false)
+        },500)
     }
 
+    useEffect(()=>{
+        setmainImage(selectedProduct.images[0].url)
+    },[])
+  
+
   return (
-    <div className='p-6'>
-        <div className='max-w-6xl mx-auto bg-white p-8 rounded-lg '>
-            <div className='flex flex-col md:flex-row '>
-                {/* Left Thumbnail */}
-                <div className='hidden md:flex flex-col space-y-4 mr-6 '>
-                    {
-                        selectedProduct.image.map((image,index)=>(
-                            <img
-                            key={index} 
-                            src={image.url}
-                             alt={image.altText || `Thumbnail ${index}`}
-                             className='w-20 h-20 object-cover rounded-lg cursor-pointer '/>
-                        ))
-                    }
+    <div className="p-6 ">
+      <div className="max-w-6xl mx-auto bg-white p-8 rounded-lg ">
+        <div className="flex flex-col md:flex-row ">
+          {/* Left Thumbnail */}
+          <div className="hidden md:flex flex-col space-y-4 mr-6 ">
+            {selectedProduct.images.map((image, index) => (
+              <img
+                key={index}
+                src={image.url}
+                alt={image.altText || `Thumbnail ${index}`}
+                className={`w-20 h-20 object-cover rounded-lg cursor-pointer border-2
+                ${mainImage === image.url ? "border-black" : "border-gray-500"}`}
+                onClick={()=> setmainImage(image.url)}
+              />
+            ))}
+          </div>
+          {/* Main Image */}
+          <div className="md:w-1/2">
+            <div className="mb-4">
+              <img
+                src={mainImage}
+                alt="Main Product"
+                className="w-full h-auto object-cover rounded-lg"
+              />
+            </div>
+          </div>
 
-                </div>
-                {/* Main Image */}
-                <div className='md:w-1/2'>
-                <div className='mb-4'>
-                    <img src={selectedProduct.image[0].url} alt="Main Product" 
-                    className='w-full h-auto object-cover rounded-lg'/>
+          {/* Mobile Thumbnail */}
+          <div className="md:hidden flex overscroll-x-scroll space-x-4 mb-4">
+            {selectedProduct.images.map((image, index) => (
+              <img
+                key={index}
+                src={image.url}
+                alt={image.altText || `Thumbnail ${index}`}
+                 className={`w-20 h-20 object-cover rounded-lg cursor-pointer border-2
+                ${mainImage === image.url ? "border-black" : "border-gray-500"}`}
+                onClick={()=> setmainImage(image.url)}
+              />
+            ))}
+          </div>
 
-                </div>
-                     
-                </div>
+          {/* Right Side */}
 
-                {/* Mobile Thumbnail */}
-                <div className='md:hidden flex overscroll-x-scroll space-x-4 mb-4'>
-                     {
-                        selectedProduct.image.map((image,index)=>(
-                            <img
-                            key={index} 
-                            src={image.url}
-                             alt={image.altText || `Thumbnail ${index}`}
-                             className='w-20 h-20 object-cover rounded-lg cursor-pointer border'/>
-                        ))
-                    }
-
-                </div>
-
-                {/* Right Side */}
-
-                <div className='md:w-1/2 md:ml-10'>
-                <h1 className='text-2xl md:text-3xl font-semibold mb-2'>
-                    {
-                        selectedProduct.name
-                    }
-                </h1>
-                <p className='text-lg text-gray-600 mb-1 line-through'>
-                   ${selectedProduct.OriginalPrice && ` ${selectedProduct.OriginalPrice}` }
-
-                </p>
-                <p className='text-xl text-gray-500 mb-2'>
-                    ${selectedProduct.price}
-                </p>
-                <p className='text-gray-600 mb-4'>
-                  {selectedProduct.description}
-                </p>
-                <div className='mb-4'>
-                    <p className='text-gray-700 '>Color</p>
-                    <div className='flex gap-2 mt-2 '>
-                        {selectedProduct.colors.map((color)=>(
-                            <button key={color} className='w-8 h-8 rounded-full border'
-                            style={{backgroundColor: color.toLocaleLowerCase(),
-                                filter:"brightness(0.5)",
-                            }}
-                            ></button>
-                        ))}
-                    </div>
-                </div>
-
-                <div className='mb-4 '>
-                    <p className='text-gray-700'>Size</p>
-                    <div className='flex gap-2 mt-2'>
-                        {selectedProduct.sizes.map((size)=>(
-                            <button key={size} className='px-4 py-2 rounded border'>
-                                {size}
-
-                            </button>
-                        ))}
-                    </div>
-
-                </div>
-
-                <div className='mb-6'>
-                    <p className='text-gray-700'>Quantity:</p>
-                    <div className='flex items-center space-x-4 mt-2'>
-                        <button className='px-2 py-1 bg-gray-200 rounded text-leg'>
-                            -
-                        </button>
-                        <span className='text-lg'>1</span>
-                        <button className='px-2 py-1 bg-gray-200 rounded text-leg'>
-                            +
-                        </button>
-
-                    </div>
-                </div>
-                <button className='bg-black text-white py-2 px-6 rounded w-full mb-4 cursor-pointer'>
-                  ADD TO CART
-                </button>
-                <div className='mt-10 text-gray-700 '>
-                    <h3 className='text-xl font-bold mb-4'>Characteristics</h3>
-                    <table className='w-full text-left text-sm text-gray-600'>
-                        <tbody>
-                            <tr>
-                                <td className='py-1'>Brand</td>
-                                <td className='py-1'>{selectedProduct.brand}</td>
-                            </tr>
-                             <tr>
-                                <td className='py-1'>Material</td>
-                                <td className='py-1'>{selectedProduct.material}</td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-
-                </div>
-
+          <div className="md:w-1/2 md:ml-10">
+            <h1 className="text-2xl md:text-3xl font-semibold mb-2">
+              {selectedProduct.name}
+            </h1>
+            <p className="text-lg text-gray-600 mb-1 line-through">
+              $
+              {selectedProduct.OriginalPrice &&
+                ` ${selectedProduct.OriginalPrice}`}
+            </p>
+            <p className="text-xl text-gray-500 mb-2">
+              ${selectedProduct.price}
+            </p>
+            <p className="text-gray-600 mb-4">{selectedProduct.description}</p>
+            <div className="mb-4">
+              <p className="text-gray-700 ">Color</p>
+              <div className="flex gap-2 mt-2 ">
+                {selectedProduct.colors.map((color) => (
+                  <button
+                    key={color}
+                    onClick={()=> setSelectColor(color)}
+                    className={`w-8 h-8 rounded-full border ${selectColor===color? "border-4 border-black": "border-gray-300"}`}
+                    style={{
+                      backgroundColor: color.toLocaleLowerCase(),
+                      filter: "brightness(0.5)",
+                    }}
+                  ></button>
+                ))}
+              </div>
             </div>
 
+            <div className="mb-4 ">
+              <p className="text-gray-700">Size</p>
+              <div className="flex gap-2 mt-2">
+                {selectedProduct.sizes.map((size) => (
+                  <button key={size}
+                  onClick={()=> setSelectSize(size)} 
+                  className={`px-4 py-2 rounded border 
+                            ${selectSize===size? "bg-black text-white":""}`}>
+                    {size}
+                  </button>
+                ))}
+              </div>
+            </div>
+
+            <div className="mb-6">
+              <p className="text-gray-700">Quantity:</p>
+              <div className="flex items-center space-x-4 mt-2">
+                <button 
+                onClick={()=> handleQuantity("minus")}
+                className="px-2 py-1 bg-gray-200 rounded text-leg">
+                  -
+                </button>
+                <span className="text-lg">{quantity}</span>
+                <button 
+                onClick={()=> handleQuantity("plus")}
+                className="px-2 py-1 bg-gray-200 rounded text-leg">
+                  +
+                </button>
+              </div>
+            </div>
+            <button
+            onClick={handleAddToCart}
+            className={`bg-black text-white py-2 px-6 rounded w-full mb-4 cursor-pointer
+                       ${isButtonDisabled? "opacity-50 cursor-not-allowed": "hover:bg-gray-900"}`}>
+              {isButtonDisabled? "Adding...": "ADD TO CART"}
+            </button>
+            <div className="mt-10 text-gray-700 ">
+              <h3 className="text-xl font-bold mb-4">Characteristics</h3>
+              <table className="w-full text-left text-sm text-gray-600">
+                <tbody>
+                  <tr>
+                    <td className="py-1">Brand</td>
+                    <td className="py-1">{selectedProduct.brand}</td>
+                  </tr>
+                  <tr>
+                    <td className="py-1">Material</td>
+                    <td className="py-1">{selectedProduct.material}</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
         </div>
+
+        {/* You may Also Like */}
+
+        <div className="mt-20">
+            <h2 className="text-2xl text-center font-medium mb-4">
+                You May Also Like
+            </h2>
+            <ProductGrid products={similarProducts}/>
+        </div>
+
+
+
+
+      </div>
     </div>
-  )
+  );
 }
 
-export default ProductDetails
+export default ProductDetails;
