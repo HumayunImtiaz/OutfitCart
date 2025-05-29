683e9bc06894bb25bc12221fc4aff71c6ad436c1
HumayunImtiaz <Humayunimtiaz81@gmail.com>
2025-05-28 23:40:39 -0700

    order confirmation UI complete

--- frontend/e-commerce/src/components/Layout/CartDrawer.jsx
+++ frontend/e-commerce/src/components/Layout/CartDrawer.jsx
@@ -6,7 +6,9 @@ import { useNavigate } from 'react-router-dom'
 function CartDrawer({drawerOpen, ToggleCartDrawer}) {
  
   const navigate = useNavigate();
+  
   const handleCheckout=()=>{
+    ToggleCartDrawer()
     navigate("/checkout")
 
   }  
